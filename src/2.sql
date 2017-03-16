9cbfadcc7b5afb72f909bb4b9605099edb21e78b
Ryadh Dahimene <ryadhdahimene@YS0350-PC-RDA.local>
2017-03-16 23:27:30 +0100

    Added queries

--- src/2.sql
+++ src/2.sql
@@ -17,10 +17,6 @@ select avg (passenger_count) from epita.taxi
 
 select avg (total_amount) as avg, min (total_amount) as min, max (total_amount) as max from epita.taxis
 
--- C'est quoi cette course à 990 $ ?!
-SELECT * from epita.taxis
-having total_amount = max(total_amount)
-
 
 -- # de Courses impliquant un Aéroport
 
@@ -71,5 +67,33 @@ ORDER BY week
 SELECT from_unixtime(unix_timestamp(date_pickup,'yyyyMMdd'),'u') as dayOfWeek, count(*)
 FROM epita.taxis
 GROUP BY from_unixtime(unix_timestamp(date_pickup,'yyyyMMdd'),'u')
+
+
+-- Trouver les 10 trajets les plus rentables ainsi que leur distances moyennes
+
+SELECT concat(neighborhood_pickup," -> " , neighborhood_dropoff) as trajet, avg(distance) as dist_moyenne, sum(total_amount) as somme
+FROM epita.taxis
+GROUP BY neighborhood_pickup, neighborhood_dropoff
+ORDER BY somme DESC
+LIMIT 10
+
+
+-- Trouver les 10 courses les plus longues ainsi que leur cout
+
+SELECT concat(neighborhood_pickup," -> " , neighborhood_dropoff) as trajet, distance as dist, total_amount as cout
+FROM epita.taxis
+ORDER BY dist DESC
+LIMIT 10
+
+
+-- Trouver les 10 trajets moyens les plus longs ainsi que leur cout moyen
+
+SELECT concat(neighborhood_pickup," -> " , neighborhood_dropoff) as trajet, avg(distance) as dist, avg(total_amount) as cout
+FROM epita.taxis
+GROUP BY neighborhood_pickup , neighborhood_dropoff
+ORDER BY dist DESC
+LIMIT 10
+
+
 
 
